2d0125dd2f5dbe10c6446f9f85bc390382199460
MaikelJonkers <m.jonkers@aagbi01.onmicrosoft.com>
2023-03-20 08:03:46 +0100

    changes to modeling

--- scripts/modeling/modeling.py
+++ scripts/modeling/modeling.py
@@ -1,16 +1,65 @@
 # Imports
+import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 import pickle
+
+
+def train_knn_model(X_train,y_train, k):
+    knn = KNeighborsClassifier(n_neighbors = k)#, metric='minkowski')
+    knn.fit(X_train,y_train)
+    return knn
+
+
+def calc_accuracy_score(y_test, yhat):
+    return accuracy_score(y_test,yhat)
+
+def calc_confusion_matrix(y_test, yhat):
+    return pd.DataFrame(confusion_matrix(y_test, yhat))
+    
+
+def knn_experiment_with_range_for_k(X_train, y_train, X_test, y_test, lower_boundary_range, upper_boundary_range):
+    df_accuracy_score = pd.DataFrame(columns=['Aantal_buren', 'Nauwkeurigheidsscore'])
+
+    for i in range(lower_boundary_range, upper_boundary_range+1):
+        knn = train_knn_model(X_train, y_train, k=i)
+        yhat = knn.predict(X_test)
+        acc = calc_accuracy_score(y_test,yhat)
+        df_accuracy_score = pd.concat([df_accuracy_score, pd.DataFrame(data=[{'Aantal_buren':i, 'Nauwkeurigheidsscore':acc}], index=[i])])
+
+    return df_accuracy_score
+
+
+def knn_experiment(df, y_column, lower_boundary_range, upper_boundary_range):
+    # Train_test_split
+    list_variables = list(set(df.columns) - set([y_column]))
+    X=df[list_variables]
+    y=df[y_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+        test_size = 0.2, random_state=1)
+    df_accuracy_score = knn_experiment_with_range_for_k(X_train, y_train, X_test, y_test, lower_boundary_range, upper_boundary_range)
+    return df_accuracy_score
+
+def knn_with_specific_neighbour(df, k, y_column):
+    # Train_test_split
+    list_variables = [e for e in df.columns if e not in [y_column]]
+    X=df[list_variables]
+    y=df[y_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+        test_size = 0.2, random_state=1)
+    knn = train_knn_model(X_train, y_train, k=k)
+    yhat = knn.predict(X_test)
+    df_confusion = calc_confusion_matrix(y_test,yhat)
+    
+    return knn, df_confusion
 
 
 def find_best_number_neighbours(X_train, y_train, X_test, y_test):
     acc = []
 
     for i in range(1,20):
-        knn = KNeighborsClassifier(n_neighbors = i)
-        knn.fit(X_train,y_train)
+        train_knn_model(X_train, y_train, k=i)
         yhat = knn.predict(X_test)
         acc.append(accuracy_score(y_test,yhat))
         print(f"For k = {i} : {accuracy_score(y_test,yhat)}")
@@ -23,7 +72,8 @@ def find_best_number_neighbours(X_train, y_train, X_test, y_test):
 def create_knn_model(df, y_column):
     
     # Train_test_split
-    list_variables = list(set(df.columns) - set([y_column]))
+    # list_variables = list(set(df.columns) - set([y_column]))
+    list_variables = [e for e in df.columns if e not in [y_column]]
     X=df[list_variables]
     y=df[y_column]
     X_train, X_test, y_train, y_test = train_test_split(X, y,
--- scripts/modeling/modeling_frontend.py
+++ scripts/modeling/modeling_frontend.py
@@ -3,12 +3,39 @@ import os
 import pandas as pd
 from pyaml_env import parse_config
 
-from scripts.modeling.modeling import create_knn_model, save_model2pickle, load_model_from_pickle
+from scripts.modeling.modeling import knn_experiment, knn_with_specific_neighbour, create_knn_model, save_model2pickle, load_model_from_pickle
 
 #Load settings
 config_path="../settings.yml"
 
 _config = parse_config(os.path.abspath(os.path.join(os.path.dirname(__file__), config_path)))
+
+def experimenteer_met_aantal_buren(df, ondergrens=1, bovengrens=20, config=_config):
+    # Exclude possible extra entries
+    df_train = df.iloc[df.index.get_level_values('Passagier_Id')<10_000].copy()
+    df_experiment = knn_experiment(df=df_train, 
+                                   y_column=config['modeling']['y_variable'],  
+                                   lower_boundary_range=ondergrens, 
+                                   upper_boundary_range=bovengrens)
+    return df_experiment
+
+
+def verdieping_specifiek_model(df, aantal_buren, config=_config):
+    # Exclude possible extra entries
+    df_train = df.iloc[df.index.get_level_values('Passagier_Id')<10_000].copy()
+    # Train model and get confusion_matrix
+    model, df_confusion_matrix = knn_with_specific_neighbour(
+        df=df_train, 
+        k=aantal_buren,
+        y_column=config['modeling']['y_variable'])
+    # Change index and column to logical names for this use case
+    multiindex_values = [('Werkelijk','Overleden'),('Werkelijk','Overleefd')]
+    columns_values = [('Voorspeld','Overleden'),('Voorspeld','Overleefd')]
+    df_confusion_matrix = df_confusion_matrix.set_index(pd.MultiIndex.from_tuples(multiindex_values))
+    df_confusion_matrix.columns = pd.MultiIndex.from_tuples(columns_values)
+    # Save model
+    save_model2pickle(model=model, default_filename=config['modeling']['filename'])
+    return df_confusion_matrix
 
 
 def train_and_save_model(df, config=_config):
